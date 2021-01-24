ed43e04be42b46c977c78e2d6915e9ee091dda38
Manoj <manoj@1gen.io>
2021-01-24 23:29:54 +0530

    second commit gvg

--- src/test/java/com/assignment/PostControllerTest.java
+++ src/test/java/com/assignment/PostControllerTest.java
@@ -8,7 +8,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import java.io.IOException;
 import java.net.URISyntaxException;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
